fb9188ec72c91cc6d1b7d75c3a9b472666659c84
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-02-23 21:25:19 -0500

    Update database

---
+++ db.sql
@@ -0,0 +1,25 @@
+-- Adminer 4.8.1 MySQL 8.3.0 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(120) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `contact` varchar(120) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `User` (`id`, `full_name`, `email`, `password`, `contact`) VALUES
+(1,	'Sebastián',	'sbs@gmail.com',	'password',	'contact'),
+(2,	'Fabián',	'lugo@gmail.com',	'password',	'contact');
+
+-- 2024-02-24 02:13:00
