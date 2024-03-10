f05bd2eaad641c435c00c8547cefd1d6a9d646e6
LugoDv <falugopersonal@gmail.com>
2024-03-09 21:04:32 -0500

    feat(model) add reservation and user with atributtes

--- src/model/User.java
+++ src/model/User.java
@@ -9,5 +9,60 @@ package model;
  * @author Fabián Lugo - Sebastián Cruz
  */
 public class User {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+    private String details;
+
+    public User(int id, String userName, String email, String password, String details) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.details = details;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
+    
     
 }
