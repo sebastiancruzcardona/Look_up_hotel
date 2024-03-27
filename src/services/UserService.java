d9239447a280cd6b370a7c76933e07bed8963591
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-03-27 16:02:23 -0500

    feat(UserService): Create the methods that call UserDAO methods

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -4,10 +4,42 @@
  */
 package services;
 
+import daos.UserDAO;
+import java.util.Map;
+
 /**
  *
  * @author ASUS - JOSE
  */
 public class UserService {
+    
+    UserDAO userDAO;
+
+    public UserService(UserDAO userDAO) {
+        userDAO = new UserDAO();
+    }
+    
+    //This method calls insert method from UserDAO
+    public void insert(String name, String email, String password, String contact){
+        userDAO.insert(name, email, password, contact);
+    }
+    
+    //This method calls update method from UserDAO
+    public void update(String name, String email, String password, String contact){
+        userDAO.update(name, email, password, contact);
+    }
+    
+    //This method returns a HashMap calling userDAO select method
+    public Map<String, Object> select(){
+        return userDAO.select();
+    }
+    
+    //This method calls delete method from UserDAO
+    public void delete(int id){
+        userDAO.delete(id);
+    }
+    
+    
+    
     
 }
