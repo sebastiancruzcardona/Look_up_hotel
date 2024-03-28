0673c070c475d45253fe4eef260fe97805d19dbf
LugoDv <falugopersonal@gmail.com>
2024-03-27 20:27:15 -0500

    feat(Exception) create new exception EmailNotExistExcpetion

---
+++ src/exceptions/EmailNotExistException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+/**
+ *
+ * @author lugo
+ */
+public class EmailNotExistException extends RuntimeException {
+
+    public EmailNotExistException() {
+        super("email does not exist in the database");
+    
+    }
+    
+    
+}
