b165793e42a2fe2ba1a22feb2a38fa4f53235085
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-04-03 17:26:03 -0500

    refactor(InsertHotel): Clean not needed imports

--- src/view/InsertHotel.java
+++ src/view/InsertHotel.java
@@ -4,14 +4,7 @@
  */
 package view;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JComboBox;
-import javax.swing.table.DefaultTableModel;
 import services.HotelService;
-import services.RoomService;
 
 /**
  *
