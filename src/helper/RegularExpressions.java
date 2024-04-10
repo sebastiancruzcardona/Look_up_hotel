034a4a60640fc4e349c7571915cd27093a0b33eb
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-04-10 15:44:04 -0500

    feat(RegularExpressions): create regular expression for email validation

--- src/helper/RegularExpressions.java
+++ src/helper/RegularExpressions.java
@@ -4,10 +4,19 @@
  */
 package helper;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author ASUS - JOSE
  */
 public class RegularExpressions {
-    
+
+    public static boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile("^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
