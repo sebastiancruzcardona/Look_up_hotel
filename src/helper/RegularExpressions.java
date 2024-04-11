6c87ab0e9c5f3174ab6e6d4c4d42734af2dd67b9
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-04-11 17:32:28 -0500

    docs(RegularExpressions): Write methods descriptions

--- src/helper/RegularExpressions.java
+++ src/helper/RegularExpressions.java
@@ -12,25 +12,29 @@ import java.util.regex.Pattern;
  * @author ASUS - JOSE
  */
 public class RegularExpressions {
-
+    
+    //This method returns true if the evaluated string is an email
     public static boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile("^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$");
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
     
+    //This method returns true if the evaluated string contains just letters (upper and lower case) and/or spaces
     public static boolean validateAlpha(String string){
         Pattern pattern = Pattern.compile("^[A-Za-z\\s]+$");
         Matcher matcher = pattern.matcher(string);
         return matcher.matches();
     }
     
+    //This method returns true if the evaluated string contains just numbers, letters (upper and lower case) and/or spaces
     public static boolean validateAlphaNumeric(String string){
         Pattern pattern = Pattern.compile("^[0-9A-Za-z\\s]+$");
         Matcher matcher = pattern.matcher(string);
         return matcher.matches();
     }
     
+    //This method returns true if the evaluated string contains just numbers
     public static boolean validateNumeric(String string){
         Pattern pattern = Pattern.compile("^[0-9]+$");
         Matcher matcher = pattern.matcher(string);
