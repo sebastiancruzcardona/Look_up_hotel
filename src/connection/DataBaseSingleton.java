cfebdd35459a9be3eec024456b27fd7780dc1194
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-04-12 04:51:23 -0500

    feat(DataBaseSingleton): Create new class DataBaseSingleton in connection package

--- src/connection/DataBaseSingleton.java
+++ src/connection/DataBaseSingleton.java
@@ -14,21 +14,25 @@ import java.sql.SQLException;
  *
  * @authors Fabián Lugo - Sebastián Cruz
  */
-public class MySQLConnection {
+public class DataBaseSingleton {
+    
+    private static DataBaseSingleton instance;
+    private Connection connection;
+    
     //MySQL library
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     //Database name
-    private static final String DATABASE = "my-first-mysql";
+    //private static final String DATABASE = "my-first-mysql";
 
     //Host
-    private static final String HOSTNAME = "localhost";
+    //private static final String HOSTNAME = "localhost";
 
     //Port
-    private static final String PORT = "3306";
+    //private static final String PORT = "3306";
 
     //Path to our database (disable SSL use with "?useSSL=false")
-    private static final String URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + DATABASE + "?useSSL=false&allowPublicKeyRetrieval=True";
+    private static final String URL = "jdbc:mysql://localhost:3306/my-first-mysql?useSSL=false&allowPublicKeyRetrieval=True";
 
     //User name
     private static final String USERNAME = "root";
@@ -36,24 +40,36 @@ public class MySQLConnection {
     //User password
     private static final String PASSWORD = "clavecita1357";
 
-    /**
-     * This method stablishes the connection to database
-     * @return Connection to database.
-     */
-    public static Connection connectMySQL() {
-        Connection conn = null;
-
+    
+    private DataBaseSingleton(){
         try {
-            //Load MySQL driver
-            Class.forName(DRIVER);
-            //Stablish connection to database
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            //If can't connect to databas print out the exception trace
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        //Return the connection
-        return conn;
+    }
+    
+    
+    public static DataBaseSingleton getInstance(){
+        if(instance == null){
+            instance = new DataBaseSingleton();
+        }
+        return instance;
+    }
+    
+    
+    public Connection getConnection(){
+        return connection;
+    }
+    
+    
+    public void closeConnection(){
+        if(connection != null){
+            try{
+                connection.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
