d06e402e9262cbc59df4f042d1d3057bd6a23e8f
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-04-12 04:50:21 -0500

    feat(DataBaseSingleton): Create new class DataBaseSingleton in connection package

---
+++ src/connection/DataBaseSingleton.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+/**
+ *
+ * @authors Fabián Lugo - Sebastián Cruz
+ */
+public class MySQLConnection {
+    //MySQL library
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    //Database name
+    private static final String DATABASE = "my-first-mysql";
+
+    //Host
+    private static final String HOSTNAME = "localhost";
+
+    //Port
+    private static final String PORT = "3306";
+
+    //Path to our database (disable SSL use with "?useSSL=false")
+    private static final String URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + DATABASE + "?useSSL=false&allowPublicKeyRetrieval=True";
+
+    //User name
+    private static final String USERNAME = "root";
+
+    //User password
+    private static final String PASSWORD = "clavecita1357";
+
+    /**
+     * This method stablishes the connection to database
+     * @return Connection to database.
+     */
+    public static Connection connectMySQL() {
+        Connection conn = null;
+
+        try {
+            //Load MySQL driver
+            Class.forName(DRIVER);
+            //Stablish connection to database
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            //If can't connect to databas print out the exception trace
+            e.printStackTrace();
+        }
+
+        //Return the connection
+        return conn;
+    }
+}
